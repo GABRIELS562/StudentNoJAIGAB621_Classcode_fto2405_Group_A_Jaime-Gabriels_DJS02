0d603e5fda8b58384e088fcca5c9f663873e1458
Gabriels562 <63159064+GABRIELS562@users.noreply.github.com>
2024-09-04 16:09:04 +0200

    chore: completed validation for missing values

--- scripts.js
+++ scripts.js
@@ -2,8 +2,30 @@ const form = document.querySelector("[data-form]");
 const result = document.querySelector("[data-result]");
 
 form.addEventListener("submit", (event) => {
-  event.preventDefault();
-  const entries = new FormData(event.target);
+  event.preventDefault();//stops the default form submission behaviour
+  
+  
+  try {
+    const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
+  
+  if (!dividend || !divider) { 
+    result.innerText =  "Division not performed. Both values are required in inputs. Try again.";
+    return; //stops the code from running if no values have been entered
+  }
+
+const divideConvertToNumber = Number(dividend);
+const dividerConvertToNumber = Number(divider); // in case a string submitted
+
+// Check if inputs are valid numbers
+if (isNaN(dividendNum) || isNaN(dividerNum)) {
+  result.innerText("Invalid number provided");
+  return
+}
+
+
+
   result.innerText = dividend / divider;
 });
+catch 
+
